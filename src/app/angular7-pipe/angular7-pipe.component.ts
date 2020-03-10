bf0b8b36d3b39badfc4b59c66d4ede15d3c184ca
Pratul Belokar <pratul.belokar@gmail.com>
2020-03-11 01:33:43 +0530

    custom-pipe

--- src/app/angular7-pipe/angular7-pipe.component.ts
+++ src/app/angular7-pipe/angular7-pipe.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Component({
   selector: 'app-angular7-pipe',
   templateUrl: './angular7-pipe.component.html',
   styleUrls: ['./angular7-pipe.component.css']
 })
-export class Angular7PipeComponent implements OnInit {
-  
-  title = 'my-first-app';  
-  todaydate = new Date();  
-  jsonval = {name: 'Alex', age: '25', address:{a1: 'Paris', a2: 'France'}};  
-  months = ['Jan', 'Feb', 'Mar', 'April', 'May', 'Jun',  
-    'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];  
+// @Pipe({
+//   name:'Angular7PipeComponent'
+// })
+export class Angular7PipeComponent implements PipeTransform {
+
+  title = 'my-first-app';
+  todaydate = new Date();
+  jsonval = { name: 'Alex', age: '25', address: { a1: 'Paris', a2: 'France' } };
+  months = ['Jan', 'Feb', 'Mar', 'April', 'May', 'Jun',
+    'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
 
   constructor() { }
+
+  transform(val: number): number {
+    return Math.sqrt(val);
+  }
 
   ngOnInit(): void {
   }
