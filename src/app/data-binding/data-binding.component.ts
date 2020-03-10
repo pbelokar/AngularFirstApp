f038b71f1cb2bde4a7c725777db020c7141fe9f5
Pratul Belokar <pratul.belokar@gmail.com>
2020-03-11 00:53:38 +0530

    EventBinding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -8,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class DataBindingComponent implements OnInit {
 
   isDisabled: boolean = true;
-  
-  constructor() { }
+
+  constructor() {
+    setTimeout(() => {
+      this.isDisabled = false;
+    }, 5000);
+  }
 
   ngOnInit(): void {
   }
